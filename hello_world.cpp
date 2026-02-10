c35b845f420f2639edbd38a34bf72f4614f9b1bf
Mehmet Alp ARSLAN <5alparslanmehmet8@gmail.com>
2026-02-10 14:39:27 +0300

    lesson_1

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*Bir tab boşluk bırakmak için kullanılır
+       \t  */
+    cout << "Selam\tCpp";
+    return 0;
+}
---
+++ lesson_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std;
+
+int main ()
+{
+    // \n alt satıra geçmek için kullanılır 
+    cout << 3 + 3 << '\n';
+    // yine alt satıra geçmek için kullanılan bir diğer yöntem
+    cout << 5 - 3 << endl;
+    cout << 3 * 3 << '\n' ;
+    return 0;
+}
