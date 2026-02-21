db781c8e40aeded40a23a903f8d5412da247fcf1
Mehmet Alp ARSLAN <5alparslanmehmet8@gmail.com>
2026-02-21 13:47:51 +0300

    3 boyutlu dizi örneği

--- Temel Özellikler/lesson_21_3lu_matris.cpp
+++ Temel Özellikler/lesson_21_3lu_matris.cpp
@@ -8,7 +8,49 @@ int main ()
 {
     selamlama();
 
+    string DiziUcBoyutlu [4][2][2] = {
+    {
+        { "A", "B" },
+        { "C", "D" }
+    },
+
+    {
+        { "E", "F" },
+        { "G", "H" }
+    },
+
+    {
+        { "I", "İ" },
+        { "J", "K" }
+    },
+
+    {
+        { "L", "M" },
+        { "N", "O" }
+    }
+    };
+
+
+
+
+
+
+    for (int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 2; j++)
+        {
+            for(int d = 0; d < 2; d++)
+            {
+                cout << DiziUcBoyutlu [i][j][d];
+
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
     
+
+
 
 
 
