8a530ee0c5c17960d3524690fa0073fb2aef456f
Mehmet Alp ARSLAN <5alparslanmehmet8@gmail.com>
2026-02-22 15:02:42 +0300

    memory_adress

---
+++ Temel Özellikler/lesson_26_memory_adress.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "selam.h" 
+
+using namespace std;
+
+int main ()
+{
+    selamlama();
+
+    string deger = "Merhaba";
+
+    cout << "deger adlı değişkenin adresi: z" << &deger << endl;
+    
+
+    veda();
+    return(0);
+}
