d796a1b9848d3d337272a639d4c764c3cf100ad5
Mehmet Alp ARSLAN <5alparslanmehmet8@gmail.com>
2026-03-12 12:51:21 +0300

    12 mart lab

---
+++ Örnekler/Ders Örnekleri/12_03_26_uyg7.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include "../selam.h" 
+
+using namespace std;
+
+void hasarAl(int *hp, int hasar);
+
+void seviyeAtla(int &hp, int &attack, int &armor, double katsayi);
+
+void swapEkipman(int &attack1, int &armor1, int &attack2, int &armor2);
+
+int* enDusukHP(int *hp1, int *hp2, int *hp3);
+
+void karakterYazdir(string isim, int hp, int attack, int armor);
+int main ()
+{
+    selamlama();
+
+
+
+
+    
+
+    veda();
+    return(0);
+}
+
+void hasarAl(int *hp, int hasar)
+{
+
+}
+
+void seviyeAtla(int &hp, int &attack, int &armor, double katsayi)
+{
+
+}
+
+void swapEkipman(int &attack1, int &armor1, int &attack2, int &armor2)
+{
+
+}
+
+int* enDusukHP(int *hp1, int *hp2, int *hp3)
+{
+
+}
+
+void karakterYazdir(string isim, int hp, int attack, int armor)
+{
+    
+}
