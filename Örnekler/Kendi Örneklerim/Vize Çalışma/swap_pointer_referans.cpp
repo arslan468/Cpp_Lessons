24ba9a4fa3129eb3c46e607a8466aa572f2b823e
Mehmet Alp ARSLAN <5alparslanmehmet8@gmail.com>
2026-04-08 09:40:25 +0300

    yeni-klasör-vize-dosyaları

---
+++ Örnekler/Kendi Örneklerim/Vize Çalışma/swap_pointer_referans.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "../selam.h"
+#include <string>
+#include <cctype>
+using namespace std;
+
+void swap_islemi(int &deger1,int &deger2);
+
+int main()
+{
+
+    int x = 3, y=2;
+
+    
+    
+    
+
+    
+}
