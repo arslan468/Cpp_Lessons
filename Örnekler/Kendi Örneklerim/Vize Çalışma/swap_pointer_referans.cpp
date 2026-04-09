e07b35a3fc73a4a6aa3e87e7890f61db8c4eee0c
Mehmet Alp ARSLAN <5alparslanmehmet8@gmail.com>
2026-04-09 18:08:50 +0300

    swap-referans

--- Örnekler/Kendi Örneklerim/Vize Çalışma/swap_pointer_referans.cpp
+++ Örnekler/Kendi Örneklerim/Vize Çalışma/swap_pointer_referans.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "../selam.h"
 #include <string>
 #include <cctype>
 using namespace std;
@@ -8,12 +7,15 @@ void swap_islemi(int &deger1,int &deger2);
 
 int main()
 {
-
     int x = 3, y=2;
+    swap_islemi(x, y);
+    return 0;
+}
 
-    
-    
-    
-
-    
+void swap_islemi(int &deger1,int &deger2){
+    int tempbakiye = deger1;
+    deger1 = deger2;
+    deger2 = tempbakiye;
+    cout << "x : " << deger1; 
+    cout << "\ny : " << deger2 << endl; 
 }
