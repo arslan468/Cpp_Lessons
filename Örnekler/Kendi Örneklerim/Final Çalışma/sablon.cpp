4c40e6cc0fbe7a33c5e608cb92906d1c045db6d9
Mehmet Alp ARSLAN <5alparslanmehmet8@gmail.com>
2026-06-09 09:57:09 +0300

    Refactor code structure for improved readability and maintainability

--- Örnekler/Kendi Örneklerim/Final Çalışma/sablon.cpp
+++ Örnekler/Kendi Örneklerim/Final Çalışma/sablon.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include "../../selam.h"
 
 using namespace std;
--- Örnekler/Kendi Örneklerim/Final Çalışma/vector_ornek4_siralama_birlestirme.cpp
+++ Örnekler/Kendi Örneklerim/Final Çalışma/vector_ornek4_siralama_birlestirme.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include "../../selam.h"
 
 using namespace std;
-
 template <typename T>
-vector<T> vectorBirlestir(const)
+vector<T> vectorBirlestir(const vector<T>&v,const vector <T>&v1)
+{
+    vector <T> birlesim;
+    for(const T &x:v)
+    {
+        birlesim.push_back(x);
+    }
+    for(size_t i=0; i<v1.size(); i++)
+    {
+        birlesim.push_back(v1[i]);
+    }
+    sort(birlesim.begin(),birlesim.end());
+
+    return birlesim;
+}
  
 int main()
 {
     selamlama();
-
-    
-
-
+    vector<int> v1 = {10, 5, 20};
+    vector<int> v2 = {2, 8};
+    vector<int> sonuc=vectorBirlestir(v1,v2);
+    for(int &x:sonuc)
+    {
+        cout << x << "\t";
+    }
 
     veda();
-
     return 0;
 }
